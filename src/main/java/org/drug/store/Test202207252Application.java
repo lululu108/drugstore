14038bb76d0234a4d51176c4bf51da6eafa29b61
DESKTOP-61RINEG\ユーザー <nemukoo@gmail.com>
2022-08-02 15:38:30 +0900

    drug_store

---
+++ src/main/java/org/drug/store/Test202207252Application.java
@@ -0,0 +1,13 @@
+package org.drug.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test202207252Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test202207252Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/drug/store/Test202207252ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.drug.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test202207252ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
